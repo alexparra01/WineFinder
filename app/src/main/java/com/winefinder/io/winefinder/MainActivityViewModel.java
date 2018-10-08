08da2ac11260fa910b474ce6414d093b817b6621
alexanderparra <alexanderparrap@gmail.com>
2018-10-08 21:14:58 +1300

    initial commit

---
+++ app/src/main/java/com/winefinder/io/winefinder/MainActivityViewModel.java
@@ -0,0 +1,13 @@
+package com.winefinder.io.winefinder;
+
+import android.arch.lifecycle.ViewModel;
+
+public class MainActivityViewModel extends ViewModel {
+
+
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+    }
+}
